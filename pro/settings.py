9934cc085838f8bd9909648a72f8bc15861b704b
wchorong <wchorong@naver.com>
2024-04-22 17:29:53 +0900

    local test

--- pro/settings.py
+++ pro/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['doday.site']
+ALLOWED_HOSTS = ['doday.site', 'www.doday.site']
 
 
 # Application definition
